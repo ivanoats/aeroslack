ddccdaa89c01118a3b37ffef5e4d60c8c8a53a62
Ivan Storck <ivanoats@gmail.com>
2015-07-02 19:39:34 -0700

    remove jquery for mobile performance

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
-$(document).ready(function() {
-  $('slack').submit(function(event) {
-    $.ajax({
-      type: 'POST',
-      url: '/slack',
-      data: {
-        'text': $('input#email').val() + ' asks: ' + $('input#message').val(),
+function ready(fn) {
+  if (document.readyState != 'loading') {
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+ready(function() {
+  document.getElementById('slack').submit(function(event) {
+    var request = new XMLHttpRequest();
+    request.open('POST', '/slack', true);
+    request.setRequestHeader(
+      'Content-Type',
+      'application/x-www-form-urlencoded; charset=UTF-8'
+    );
+
+    request.onload = function() {
+      if (this.status >= 200 && this.status < 400) {
+        var data = JSON.parse(this.response);
+      } else {
+        alert('Error with API Endpoint' + this.repsonse);
+      }
+    };
+
+    request.onerror = function() {
+      alert('Connection Error with API Endpoint')
+    };
+
+    var data = {
+      'text': $('input#email').val() + ' asks: ' + $('input#message').val(),
         'username': $('input#name').val()
-      },
-      dataType: 'json',
-      encode: true
-    })
-      .done(function(data) {
-        alert('Your message was posted to our Slack channel.'
-            + ' Check your email for a response');
-      });
-    event.preventDefault();
+    };
+    request.send(data);
+
   });
 });
