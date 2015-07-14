e3d873bebea5ed74fbb8f886fffac716bf6b2be9
Ivan Storck <ivanoats@gmail.com>
2015-07-14 12:42:07 -0400

    vanilla js working, endpoint undefined

--- main.js
+++ main.js
@@ -7,31 +7,32 @@ function ready(fn) {
 }
 
 ready(function() {
-  document.getElementById('slack').submit(function(event) {
-    var request = new XMLHttpRequest();
-    request.open('POST', '/slack', true);
-    request.setRequestHeader(
-      'Content-Type',
-      'application/x-www-form-urlencoded; charset=UTF-8'
-    );
+   document.getElementById('slack')
+     .addEventListener('submit', function(event) {
+       console.log('submitted');
+       var request = new XMLHttpRequest();
+       request.open('POST', '/slack', true);
+       request.setRequestHeader(
+         'Content-Type',
+         'application/x-www-form-urlencoded; charset=UTF-8'
+       );
 
-    request.onload = function() {
-      if (this.status >= 200 && this.status < 400) {
-        var data = JSON.parse(this.response);
-      } else {
-        alert('Error with API Endpoint' + this.repsonse);
-      }
-    };
+       request.onload = function() {
+         if (this.status >= 200 && this.status < 400) {
+           var data = JSON.parse(this.response);
+         } else {
+           alert('Error with API Endpoint' + this.repsonse);
+         }
+       };
 
-    request.onerror = function() {
-      alert('Connection Error with API Endpoint')
-    };
+       request.onerror = function() {
+         alert('Connection Error with API Endpoint')
+       };
 
-    var data = {
-      'text': $('input#email').val() + ' asks: ' + $('input#message').val(),
-        'username': $('input#name').val()
-    };
-    request.send(data);
-
-  });
+       var data = {
+         'text': $('input#email').val() + ' asks: ' + $('input#message').val(),
+         'username': $('input#name').val()
+       };
+       request.send(data);
+     }, false);
 });
