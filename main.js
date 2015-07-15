3a7b22d293f8f9051d46c19f2b5e1bb319b6ab70
Ivan Storck <ivanoats@gmail.com>
2015-07-15 11:48:59 -0400

    moving back to main to test slack webhook proxy

--- main.js
+++ main.js
@@ -1,41 +1,52 @@
-function ready(fn) {
+var ready = function(fn) {
   if (document.readyState != 'loading') {
     fn();
   } else {
     document.addEventListener('DOMContentLoaded', fn);
   }
-}
+};
+
+var checkLoad = function() {
+  if (this.status >= 200 && this.status < 400) {
+    document.getElementById('slack').style.display = 'none';
+    document.getElementById('confirm').style.display = 'block';
+  } else {
+    alert('Error with API Endpoint\n' + this.repsonse);
+  }
+};
+
+var ajaxError = function() {
+  alert('Connection Error with API Endpoint');
+};
+
+var processFormData = function() {
+  return JSON.stringify({
+    'text': document.getElementById('email')
+    .value + ' asks: ' + document.getElementById('message').value,
+    'username': document.getElementById('name').value,
+    'icon_emoji': ':slack:'
+  });
+};
+
+var submitter = function(event) {
+  var request = new XMLHttpRequest();
+
+  request.open('POST',
+    '/slack',
+    true);
+
+  request.setRequestHeader(
+    'Content-Type',
+    'application/x-www-form-urlencoded; charset=UTF-8'
+  );
+
+  request.onload = checkLoad;
+  request.onerror = ajaxError;
+  request.send(processFormData());
+  event.preventDefault();
+};
 
 ready(function() {
    document.getElementById('slack')
-     .addEventListener('submit', function(event) {
-       console.log('submitted');
-       var request = new XMLHttpRequest();
-       request.open('POST', '/slack', true);
-       request.setRequestHeader(
-         'Content-Type',
-         'application/x-www-form-urlencoded; charset=UTF-8'
-       );
-
-       request.onload = function() {
-         if (this.status >= 200 && this.status < 400) {
-           var data = JSON.parse(this.response);
-         } else {
-           alert('Error with API Endpoint\n' + this.repsonse);
-         }
-       };
-
-    request.onerror = function() {
-      alert('Connection Error with API Endpoint');
-    };
-
-    var data = {
-      'text': document.getElementById('email')
-        .value + ' asks: '
-        + document.getElementById('message').value,
-        'username': document.getElementById('name').value
-    };
-    request.send(data);
-
-  });
+     .addEventListener('submit', submitter);
 });
